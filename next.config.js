064a8123e98e99a495364eb56ec1a0b8706c8f39
owilliams320 <oseer.williams@gmail.com>
2024-08-08 14:50:18 -0400

    Initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+// next.config.js
+const withLitSSR = require('@lit-labs/nextjs')();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Add your own config here
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+module.exports = withLitSSR(nextConfig);
